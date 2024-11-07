2320547fd76b98db4234b48142e0ed5d811950b7
TheOr_da <135665465+arandomguyhi@users.noreply.github.com>
2024-11-07 12:29:30 -0300

    Update BWShader.hx

--- source/shaders/BWShader.hx
+++ source/shaders/BWShader.hx
@@ -26,7 +26,7 @@ import flixel.system.FlxAssets.FlxShader;
     uniform float u_colorFactor;
 
     void main() {
-        vec4 sample = flixel_texture2D(bitmap, openfl_TextureCoordv);
+        vec4 sample = texture2D(bitmap, openfl_TextureCoordv);
         float gray = 0.21 * sample.r + 0.71 * sample.g + 0.07 * sample.b;
         gl_FragColor = vec4(sample.rgb * (1.0 - u_colorFactor) + (gray * u_colorFactor), sample.a);
     }
