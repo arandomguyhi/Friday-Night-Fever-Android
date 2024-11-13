ce3d7fa290b79651c194a34a68ad83dc933193e7
TheOr_da <135665465+arandomguyhi@users.noreply.github.com>
2024-11-12 21:39:23 -0300

    cu

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -126,7 +126,7 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 
 		box = new FlxSprite(0, 460).loadGraphic(Paths.image("dialogue/box", "shared"));
 		box.updateHitbox();
-		box.screenCenter(0x01);
+		box.screenCenter(X);
 		box.antialiasing = true;
 		add(box);
 
